46581c6465a8833efc3b44cda08a2129a32b7023
Bruno Lima <bruno.lima.dev@outlook.com>
2024-07-29 19:34:32 -0300

    feat: initial commit

---
+++ src/main/java/br/com/fiap/gatewaymanagement/GatewayManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.gatewaymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/fiap_hackathon_gateway_management/FiapHackathonGatewayManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiap_hackathon_gateway_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapHackathonGatewayManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
